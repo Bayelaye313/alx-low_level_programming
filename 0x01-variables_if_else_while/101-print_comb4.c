5fb55b9f434b638f3249a8d0e5a35e7425e1799c
root <root@DESKTOP-P2J22I8>
2023-08-12 00:05:51 +0000

    testons

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	int num1, num2, num3;
 
-	for (num1 = 0; num <= 7; num1++)
+	for (num1 = 0; num1 <= 7; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 8; num2++)
 		{
@@ -21,9 +21,6 @@ int main(void)
 				putchar(num2 + '0');
 				putchar(num3 + '0');
 		}
-	}
-	if (num1 != 7 || num2 != 8 || num3 != 9)
-	{
 		putchar(',');
 		putchar(' ');
 	}
