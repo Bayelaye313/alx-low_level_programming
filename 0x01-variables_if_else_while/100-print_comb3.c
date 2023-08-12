f8d58b94c8116e27b9d5f5cc17b32cc19f2f499c
root <root@DESKTOP-P2J22I8>
2023-08-12 00:40:31 +0000

    combend

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,12 +15,14 @@ int main(void)
 	for (num1 = 0; num1 <= 8; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 9; num2++)
-		putchar(num1 + '0');
-		putchar(num2 + '0');
-		if (num1 != 8 || num2 !=9)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			if (num1 != 8 || num2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
