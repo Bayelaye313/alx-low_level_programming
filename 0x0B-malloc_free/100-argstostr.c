e169cd8aaffca5d2c2f297563bfddbffe8b3f6f4
Bayelaye313 <bayelaye0510@gmail.com>
2023-09-04 18:46:04 +0000

    lastons

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,34 +9,34 @@
  */
 char *argstostr(int ac, char **av)
 {
-    int i, j, total_length = 0, k = 0;
-    char *result;
+	int i, j, total_length = 0, k = 0;
+	char *result;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-        {
-            total_length++;
-        }
-        total_length++;
-    }
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			total_length++;
+		}
+		total_length++;
+	}
 
-    result = (char *)malloc(sizeof(char) * (total_length + 1));
-    if (result == NULL)
-        return (NULL);
+	result = (char *)malloc(sizeof(char) * (total_length + 1));
+	if (result == NULL)
+		return (NULL);
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-        {
-            result[k++] = av[i][j];
-        }
-        result[k++] = '\n';
-    }
-    result[k] = '\0';
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			result[k++] = av[i][j];
+		}
+		result[k++] = '\n';
+	}
+	result[k] = '\0';
 
-    return (result);
+	return (result);
 }
