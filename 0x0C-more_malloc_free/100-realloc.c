8e9fb56a2d24dd5c98b3b6e3d00bfd5b58229ad7
Bayelaye313 <bayelaye0510@gmail.com>
2023-09-07 15:43:12 +0000

    bonus1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,12 +36,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
     if (new_size < old_size)
     {
         for (i = 0; i < new_size; i++)
+        {
             new_ptr[i] = old_ptr[i];
+        }
     }
     else
     {
         for (i = 0; i < old_size; i++)
+        {
             new_ptr[i] = old_ptr[i];
+        }
     }
 
     free(ptr);
