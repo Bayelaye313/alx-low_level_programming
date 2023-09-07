9f3f164f939e664d63e33e27578ef859e8ce9068
Bayelaye313 <bayelaye0510@gmail.com>
2023-09-07 15:26:59 +0000

    task2.0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,9 +15,8 @@ char *_memset(char *dest, char byte, unsigned int n)
 
     for (i = 0; i < n; i++)
     {
-        dest[i] = byte;
+	    dest[i] = byte;
     }
-
     return (dest);
 }
 
@@ -30,17 +29,13 @@ char *_memset(char *dest, char byte, unsigned int n)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *ptr;
+	char *ptr;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-
-    ptr = malloc(size * nmemb);
-
-    if (ptr == NULL)
-        return (NULL);
-
-    _memset(ptr, 0, nmemb * size);
-
-    return (ptr);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+	return (ptr);
 }
