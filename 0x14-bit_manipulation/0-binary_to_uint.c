9d63accf4652dbd6c85125d4768ab651fe1756be
Bayelaye313 <bayelaye0510@gmail.com>
2023-09-28 20:47:47 +0000

    0bits

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int bit_position = 0;
+	int i = 0;
 	unsigned int result = 0;
 
 	if (!b)
 		return (0);
 
-	while (b[bit_position] != '\0')
-		bit_position++;
-
-	for (int i = bit_position - 1; i >= 0; i--)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] == '1')
-			result |= (1 << (bit_position - 1 - i));
-		else if (b[i] != '0')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		result = 2 * result + (b[i] - '0');
 	}
 
 	return (result);
