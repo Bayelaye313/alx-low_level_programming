12e138da98adb7492f3bca2b4786104f7eea0dce
Bayelaye313 <bayelaye0510@gmail.com>
2023-09-28 22:38:45 +0000

    lasteur

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,28 +6,21 @@
  */
 void print_binary(unsigned long int n)
 {
-        unsigned long int current_bit;
-        int i;
+	int i, bit_count = 0;
+	unsigned long int current_bit;
 
-        /* Gérez le cas où n est 0 */
-        if (n == 0)
-        {
-                _putchar('0');
-                return;
-        }
+	for (i = 63; i >= 0; i--)
+	{
+		current_bit = n >> i;
 
-        for (i = 63; i >= 0; i--)
-        {
-                current_bit = n >> i;
-
-                /* Vérifiez si le bit actuel est 1 ou 0 */
-                if (current_bit & 1)
-                {
-                        _putchar('1');
-                }
-                else
-                {
-                        _putchar('0');
-                }
-        }
+		if (current_bit & 1)
+		{
+			_putchar('1');
+			bit_count++;
+		}
+		else if (bit_count)
+			_putchar('0');
+	}
+	if (!bit_count)
+		_putchar('0');
 }
