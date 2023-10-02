d9ba1008cb4aee725f97aa05d21db79a42edf6f8
Bayelaye313 <bayelaye0510@gmail.com>
2023-10-02 14:53:55 +0000

    task1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,19 +12,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t fd;
-	ssize_t bytes_read;
-	ssize_t bytes_written;
-	
-	if (filename == NULL)
-		return (0);
+	ssize_t w;
+	ssize_t t;
+
+	if(filename == NULL)
+		return (0);	
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	bytes_read = read(fd, buf, letters);
-	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-	
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
 	free(buf);
 	close(fd);
-	return (bytes_written);
+	return (w);
 }
