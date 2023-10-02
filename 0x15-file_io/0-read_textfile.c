a4414753fbde106017659871f6c14151cf6915dc
Bayelaye313 <bayelaye0510@gmail.com>
2023-10-02 15:03:11 +0000

    taskk

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,20 +1,23 @@
 #include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
+ * read_textfile - Read text file print to STDOUT.
  * @filename: text file being read
  * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * Return: w - actual number of bytes read and printed
+ *         0 when function fails or filename is NULL.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
-	
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
