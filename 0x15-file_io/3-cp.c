a05d24699fd5d26a182e576b19ec16d37897a05e
Bayelaye313 <bayelaye0510@gmail.com>
2023-10-02 21:02:37 +0000

    leufffff

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file);
+#define BUFFER_SIZE 1024
+
+char *_buffer(char *file);
 void close_file(int fd);
 
 /**
@@ -11,11 +13,9 @@ void close_file(int fd);
  *
  * Return: A pointer to the newly-allocated buffer.
  */
-char *create_buffer(char *file)
+char *_buffer(char *file)
 {
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
+	char *buffer = malloc(BUFFER_SIZE);
 
 	if (buffer == NULL)
 	{
@@ -33,11 +33,9 @@ char *create_buffer(char *file)
  */
 void close_file(int fd)
 {
-	int c;
 
-	c = close(fd);
-
-	if (c == -1)
+	if (close(fd) == -1)
+		
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -58,7 +56,7 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
+	int src, dest, r, w;
 	char *buffer;
 
 	if (argc != 3)
@@ -67,13 +65,13 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = _buffer(argv[2]);
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);	
+	src = open(argv[1], O_RDONLY);
+	r = read(src, buffer, 1024);
 
 	do {
-		if (from == -1 || r == -1)
+		if (src == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
@@ -81,8 +79,8 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		w = write(dest, buffer, r);
+		if (dest == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
@@ -90,14 +88,13 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+		dest = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
 	free(buffer);
-	close_file(from);
-	close_file(to);
+	close_file(src);
+	close_file(dest);
 
 	return (0);
 }
