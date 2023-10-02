d2ec762fa7370863e1f166a61531e512f9fe6f2a
Bayelaye313 <bayelaye0510@gmail.com>
2023-10-02 15:17:49 +0000

    task02

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,12 +21,12 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	w = write(o, text_content, len);
+	w = write(fd, text_content, len);
 
 	if (w == -1)
 		return (-1);
 
-	close(o);
+	close(fd);
 
 	return (1);
 }
