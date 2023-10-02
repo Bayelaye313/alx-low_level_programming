4b7de5dbb42f16177c3251aef5dd2f7dc496f1cc
Bayelaye313 <bayelaye0510@gmail.com>
2023-10-02 17:58:35 +0000

    task33

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,97 +1,99 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 #define BUFFER_SIZE 1024
 
-char *allocate_buffer(void);
-void close_file_descriptor(int fd);
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
  * allocate_buffer - Allocates memory for a buffer.
  *
  * Return: A pointer to the allocated buffer.
  */
-char *allocate_buffer(void)
+char *create_buffer(char *file)
 {
 	char *buffer = malloc(BUFFER_SIZE);
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Unable to allocate buffer memory\n");
-		exit(EXIT_FAILURE);
+		dprintf(STDERR_FILENO,
+			"Error: Unable to allocate buffer memory\n", file);
+		exit(99);
 	}
+
 	return (buffer);
 }
-
 /**
  * close_file_descriptor - Closes a file descriptor.
  *
  * @fd: The file descriptor to be closed.
  */
-void close_file_descriptor(int fd)
+void close_file(int fd)
 {
+
 	if (close(fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Unable to close file descriptor %d\n", fd);
-		exit(EXIT_FAILURE);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
 	}
 }
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+ */
 int main(int argc, char *argv[])
 {
-	int src_fd, dest_fd, t, w;
+	int src, dest, r, w;
 	char *buffer;
 
-/** Check if the correct number of arguments is provided */
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(EXIT_FAILURE);
-	}
-	buffer = allocate_buffer();
-
-/** Open source file for reading */
-	src_fd = open(argv[1], O_RDONLY);
-	if (src_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		free(buffer);
-		exit(EXIT_FAILURE);
-	}
-/** Open destination file for writing (truncate if it already exists) */
-	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	if (dest_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		free(buffer);
-		close_file_descriptor(src_fd);
-		exit(EXIT_FAILURE);
+		exit(97);
 	}
 
-/** Copy data from source to destination */
-	do
-	{
-		t = read(src_fd, buffer, BUFFER_SIZE);
-		if (t == -1)
+	buffer = create_buffer();
+	src = open(argv[1], O_RDONLY);
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	r = read(src, buffer, 1024);
+
+	do {
+		w = write(dest, buffer, r);	
+		if (src == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Unable to read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
-			close_file_descriptor(src_fd);
-			close_file_descriptor(dest_fd);
-			exit(EXIT_FAILURE);
+			exit(98);
 		}
-		w = write(dest_fd, buffer, t);
-		if (w == -1)
+
+		if (dest == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Unable to write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
-			close_file_descriptor(src_fd);
-			close_file_descriptor(dest_fd);
-			exit(EXIT_FAILURE);
+			exit(99);
 		}
-	} while (t > 0);
+
+		r = read(src, buffer, 1024);
+		dest = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
 	free(buffer);
-/** Close file descriptors */
-	close_file_descriptor(src_fd);
-	close_file_descriptor(dest_fd);
-	return EXIT_SUCCESS;
+	close_file(src);
+	close_file(dest);
+
+	return (0);
 }
